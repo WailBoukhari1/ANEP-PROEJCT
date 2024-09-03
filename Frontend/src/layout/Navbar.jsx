7c2d75b982cf53bfdf3e71c15d1a425f5ab6a85e
Wail Boukhari <166010597+WailBoukhari1@users.noreply.github.com>
2024-09-03 13:56:50 +0100

    Update Navbar.jsx

--- Frontend/src/layout/Navbar.jsx
+++ Frontend/src/layout/Navbar.jsx
@@ -157,7 +157,7 @@ function Navbar() {
                           : "nav-link text-gray-700 hover:text-blue-700 transition duration-300 px-4 py-3 rounded-lg font-semibold hover:bg-blue-100"
                       }
                     >
-                      Mot du DG
+                      Mot du La DG
                     </NavLink>
                   </li>
                   <li className="nav-item">
