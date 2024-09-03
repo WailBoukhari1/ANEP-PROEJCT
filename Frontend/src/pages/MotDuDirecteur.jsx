a3ec43227652561026c50b6b0d03910136c190d1
Wail Boukhari <166010597+WailBoukhari1@users.noreply.github.com>
2024-09-03 16:05:06 +0100

    Update MotDuDirecteur.jsx

--- Frontend/src/pages/MotDuDirecteur.jsx
+++ Frontend/src/pages/MotDuDirecteur.jsx
@@ -19,34 +19,6 @@ const MotDuDirecteur = () => {
 
     return (
         <MainLayout>
-            <div style={{
-                padding: '40px',
-                fontFamily: 'Arial, sans-serif',
-                border: '2px solid #e0e0e0',
-                borderRadius: '10px',
-                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                backgroundColor: '#f9f9f9'
-            }}>
-                <div style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    marginBottom: '20px',
-                    borderBottom: '2px solid #e0e0e0',
-                    paddingBottom: '20px'
-                }}>
-                    <img
-                        src="https://www.anep.ma/sites/default/files/Screenshot%20from%202023-08-19%2017-58-46.png"
-                        alt="Image ANEP"
-                        style={{
-                            width: '300px',
-                            height: 'auto',
-                            marginRight: '20px',
-                            border: '2px solid #ccc',
-                            borderRadius: '10px',
-                            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
-                        }}
-                    />
-            </div>
         </MainLayout>
     );
 };
